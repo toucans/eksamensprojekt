90fc345f5e69e8b1c196d6b47d39db76fab7378b
MarcusTheCode <MarcusTheCode@users.noreply.github.com>
2020-02-27 18:40:29 +0100

    #4

--- Databank_Eksamens_Projekt/LogIn.cs
+++ Databank_Eksamens_Projekt/LogIn.cs
@@ -6,7 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 using System.Windows.Forms;
+
 
 namespace Databank_Eksamens_Projekt
 {
@@ -15,6 +17,41 @@ namespace Databank_Eksamens_Projekt
         public LogIn()
         {
             InitializeComponent();
+        }
+
+        private void ButtonNewUser_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //DialogResult drl = new DialogResult();
+                DialogResult result = MessageBox.Show("Sure you wan't to create a new account?", "UserCreation", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    string UserNameInput = Interaction.InputBox("Please Scelect Your Username:","UsernameSeclection");
+                    string PasswordInput = Interaction.InputBox("Please Scelect Your Password:", "PasswordSeclection");
+                    DialogResult ConResult = MessageBox.Show("Confirmation of information:\n Username: "+UserNameInput+"\n Password: "+PasswordInput+"","Confirmation",MessageBoxButtons.YesNo);
+                    if (ConResult==DialogResult.Yes)
+                    {
+                        //Create user
+                        MessageBox.Show("Creaationg user");
+                    }
+                    else
+                    {
+                        throw new System.Exception();
+                    }
+                }
+                else
+                {
+                    //do nothing
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Ups, Something went wrong","Error");
+            }
+
+
         }
     }
 }
