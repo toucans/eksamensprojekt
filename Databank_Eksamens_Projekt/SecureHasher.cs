1bdd0e031b7df169dc09a8c693e9a777e76fe61a
MarcusTheCode <MarcusTheCode@users.noreply.github.com>
2020-03-13 10:37:22 +0100

    #Hash

---
+++ Databank_Eksamens_Projekt/SecureHasher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databank_Eksamens_Projekt
+{
+    public static class SecureHasher
+    {
+        private const int SaltSize = 16;
+        private const int HashSize = 20;
+
+
+        public static string Hash(string password, int interactions)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[SaltSize]);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, interactions);
+            var hash = pbkdf2.GetBytes(HashSize);
+
+            var hashBytes = new byte[SaltSize + HashSize];
+            Array.Copy(salt, 0, hashBytes, 0, SaltSize);
+            Array.Copy(hash, 0, hashBytes, SaltSize, HashSize);
+
+            var base64Hash = Convert.ToBase64String(hashBytes);
+
+            return string.Format("$MYHASH$V1${0}${1}", interactions, base64Hash);
+        }
+
+        public static string Hash(string password)
+        {
+            return Hash(password, 10000);
+        }
+        /*
+        public static bool Verify(string password, string hashedPassword)
+        {
+            if (!IsHashSupported(hashedPassword));
+            {
+
+            }
+        }
+        */
+    }
+
+}
