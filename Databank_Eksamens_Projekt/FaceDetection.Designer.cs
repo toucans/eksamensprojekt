ea58e2dcdd69b8f2667f3f405098b0c9c9dd736d
Lagzer <55091910+Lagzer@users.noreply.github.com>
2020-03-18 13:08:54 +0100

    Face detection Form
    Fik sat selve formen in til face detection nu skal den bare blive sat sammen med login :)

---
+++ Databank_Eksamens_Projekt/FaceDetection.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Databank_Eksamens_Projekt
+{
+    partial class FaceDetection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDetect = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.comboBoxDevice = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDetect
+            // 
+            this.buttonDetect.Location = new System.Drawing.Point(704, 50);
+            this.buttonDetect.Name = "buttonDetect";
+            this.buttonDetect.Size = new System.Drawing.Size(84, 42);
+            this.buttonDetect.TabIndex = 0;
+            this.buttonDetect.Text = "&Detect";
+            this.buttonDetect.UseVisualStyleBackColor = true;
+            this.buttonDetect.Click += new System.EventHandler(this.buttonDetect_Click);
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(12, 50);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(686, 388);
+            this.pic.TabIndex = 1;
+            this.pic.TabStop = false;
+            // 
+            // comboBoxDevice
+            // 
+            this.comboBoxDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDevice.FormattingEnabled = true;
+            this.comboBoxDevice.Location = new System.Drawing.Point(258, 12);
+            this.comboBoxDevice.Name = "comboBoxDevice";
+            this.comboBoxDevice.Size = new System.Drawing.Size(334, 24);
+            this.comboBoxDevice.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choose device";
+            // 
+            // FaceDetection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxDevice);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.buttonDetect);
+            this.Name = "FaceDetection";
+            this.Text = "FaceDetection using webcam";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FaceDetection_FormClosed);
+            this.Load += new System.EventHandler(this.FaceDetection_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDetect;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.ComboBox comboBoxDevice;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Databank_Eksamens_Projekt/FaceDetection.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Databank_Eksamens_Projekt
+{
+    public partial class FaceDetection : Form
+    {
+        public FaceDetection()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection filter;
+        VideoCaptureDevice device;
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml"); // This file is copy from this link "https://github.com/opencv/opencv/tree/master/data/haarcascades"
+
+        private void FaceDetection_Load(object sender, EventArgs e)
+        {
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filter)
+            {
+                comboBoxDevice.Items.Add(device.Name);
+            }
+            comboBoxDevice.SelectedIndex = 0;
+            device = new VideoCaptureDevice();
+
+        }
+
+        private void buttonDetect_Click(object sender, EventArgs e)
+        {
+            device = new VideoCaptureDevice(filter[comboBoxDevice.SelectedIndex].MonikerString);
+            device.NewFrame += Device_NewFrame;
+            device.Start();
+        }
+
+        private void Device_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+            Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.2, 1);
+            foreach (Rectangle rectangle in rectangles)
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Red, 1))
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+                    }
+
+                }
+            }
+            pic.Image = bitmap;
+        }
+
+        private void FaceDetection_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (device.IsRunning)
+            {
+                device.Stop();
+            }
+        }
+    }
+}
